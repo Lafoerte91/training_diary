d0077bc96c45787d4244f99cabfb28c1adaa4a68
Lafoerte91 <lafoerte@gmail.com>
2024-05-03 21:11:24 +0300

    form validation

--- script.js
+++ script.js
@@ -57,14 +57,10 @@ class Cycling extends Workout {
   }
 }
 
-const run1 = new Running([39, -12], 5.2, 24, 178)
-const cycling1 = new Cycling([39, -12], 27, 95, 523)
-
-console.log(run1, cycling1)
-
 class App {
   _map
   _mapEvent
+  _workouts = []
   constructor() {
     this._getPosition()
     form.addEventListener("submit", this._newWorkout.bind(this))
@@ -101,9 +97,44 @@ class App {
   }
   _newWorkout(e) {
     e.preventDefault();
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+    const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp))
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0)
+
+    // данные из формы
+    const type = inputType.value
+    const distance = +inputDistance.value
+    const duration = +inputDuration.value
+
     const {lat, lng} = this._mapEvent.latlng
-    L.marker([lat, lng]).addTo(this._map)
+    let workout
+
+    if(type == "running") {
+      const cadence = +inputCadence.value
+      if(!validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence)) {
+        return alert("Необходимо ввести целое положительное число")
+      }
+      workout = new Running([lat, lng], distance, duration, cadence)
+      this._workouts.push(workout)
+    }
+
+    if(type == "cycling") {
+      const elevation = +inputElevation.value
+      if(!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) {
+        return alert("Необходимо ввести целое положительное число")
+      }
+      workout = new Cycling([lat, lng], distance, duration, elevation)
+      this._workouts.push(workout)
+    }
+
+    // очистка полей ввода
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+
+    // рендер маркера тренировки на карте
+    this.renderWorkMarker(workout)
+    
+  }
+  renderWorkMarker(workout) {
+    L.marker(workout.coords).addTo(this._map)
     .bindPopup(L.popup({
       maxWidth: 250,
       minWidth: 100,
@@ -111,7 +142,7 @@ class App {
       closeOnClick: false,
       className: 'mark-popup'
     }))
-    .setPopupContent('Тренировка')
+    .setPopupContent(String(workout.distanse))
     .openPopup();
   }
 }
